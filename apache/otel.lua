6b6e3aa317bad4148a69424e0b5ed537dddde48a
Marc Bui <mqbui1@gmail.com>
2026-02-10 11:09:03 -0800

    Update otel.lua

--- apache/otel.lua
+++ apache/otel.lua
@@ -1,63 +1,28 @@
--- otel.lua
 local http = require("socket.http")
-local ltn12 = require("ltn12")
 local json = require("dkjson")
 
--- OTEL Collector HTTP endpoint
-local OTEL_COLLECTOR = "http://otel-collector:4318/v1/traces"
-
--- Random hex generator for trace/span IDs
-local function random_hex(n)
-    local s = ""
-    for i = 1, n do
-        s = s .. string.format("%x", math.random(0, 15))
-    end
-    return s
-end
-
--- Send trace
-local function send_trace(name, method, url)
+function log(r)
     local trace = {
-        resourceSpans = {{
-            resource = { attributes = {{ key = "service.name", value = { stringValue = "apache-demo" }}} },
-            scopeSpans = {{
-                scope = { name = "lua-otel", version = "0.1" },
-                spans = {{
-                    name = name,
-                    kind = 1, -- SERVER
-                    traceId = random_hex(32),
-                    spanId = random_hex(16),
-                    startTimeUnixNano = os.time() * 1e9,
-                    endTimeUnixNano = (os.time() + 0.001) * 1e9,
-                    attributes = {
-                        { key = "http.method", value = { stringValue = method }},
-                        { key = "http.url", value = { stringValue = url }}
-                    }
-                }}
-            }}
-        }}
+        name = r.uri,
+        kind = 1,  -- SERVER
+        startTimeUnixNano = os.clock() * 1e9,
+        endTimeUnixNano = os.clock() * 1e9 + 1000,  -- placeholder
+        attributes = {
+            { key = "http.method", value = { stringValue = r.method } },
+            { key = "http.url", value = { stringValue = r.uri } }
+        }
     }
 
-    local body = json.encode(trace)
-    local resp = {}
-    local ok, err = http.request{
-        url = OTEL_COLLECTOR,
+    -- Print to Apache log
+    r:warn("OTEL TRACE: " .. json.encode(trace))
+
+    -- Send to OTEL collector
+    local body = json.encode({ spans = { trace } })
+    local _, code = http.request{
+        url = "http://otel-collector:4318/v1/traces",
         method = "POST",
-        headers = {
-            ["Content-Type"] = "application/json",
-            ["Content-Length"] = tostring(#body)
-        },
+        headers = { ["Content-Type"] = "application/json" },
         source = ltn12.source.string(body),
-        sink = ltn12.sink.table(resp)
+        sink = ltn12.sink.null()
     }
-
-    if not ok then
-        r:warn("OTEL trace failed: " .. tostring(err))
-    end
-end
-
--- Hook called by Apache Lua
-function trace_request(r)
-    send_trace(r.uri, r.method, r.uri)
-    return 0 -- must return numeric
 end
