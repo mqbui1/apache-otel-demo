e01342584ac31f4c251fc7e34215d7341aa81a02
Marc Bui <mqbui1@gmail.com>
2026-02-10 10:47:03 -0800

    Update otel.lua

--- apache/otel.lua
+++ apache/otel.lua
@@ -1,46 +1,45 @@
 -- otel.lua
--- Apache Lua script for OpenTelemetry-style tracing
--- Place in /usr/local/apache2/otel/otel.lua
+local socket = require("socket")
+local dkjson = require("dkjson")
 
-local apache2 = require "apache2"
-local socket = require "socket"      -- optional, for timestamps
-local json = require "dkjson"        -- optional, for JSON formatting
+-- Simple trace/span ID generator
+local function random_hex(len)
+    local s = ""
+    for i = 1, len do
+        s = s .. string.format("%x", math.random(0, 15))
+    end
+    return s
+end
 
--- Simple in-memory trace ID generator
 local function generate_trace_id()
-    local random = math.random
-    local template ='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-    return string.gsub(template, 'x', function ()
-        return string.format('%x', random(0, 15))
-    end)
+    return random_hex(32)
 end
 
 local function generate_span_id()
-    local random = math.random
-    local template ='xxxxxxxxxxxxxxxx'
-    return string.gsub(template, 'x', function ()
-        return string.format('%x', random(0, 15))
-    end)
+    return random_hex(16)
 end
 
--- Main hook function for LuaHookFixups
+-- Function called by Apache on each request in fixups phase
 function trace_request(r)
+    -- Gather request info
     local method = r.method
     local uri = r.uri
     local trace_id = generate_trace_id()
     local span_id = generate_span_id()
     local timestamp = socket.gettime()
 
-    -- Log to Apache error log
-    r:err(string.format(
-        "OTEL TRACE: trace_id=%s span_id=%s method=%s uri=%s timestamp=%.6f",
-        trace_id, span_id, method, uri, timestamp
-    ))
+    -- Build log message
+    local log_entry = {
+        trace_id = trace_id,
+        span_id = span_id,
+        method = method,
+        uri = uri,
+        timestamp = timestamp
+    }
 
-    -- Optional: you could push to OTEL collector over HTTP/gRPC here
-    -- Example (pseudo-code):
-    -- send_to_otlp_collector({trace_id=trace_id, span_id=span_id, method=method, uri=uri, ts=timestamp})
+    -- Log as JSON to Apache error log
+    r:err("OTEL TRACE: " .. dkjson.encode(log_entry))
 
-    -- Return numeric status to Apache (required)
-    return apache2.OK
+    -- Return numeric status for Apache (0 = OK)
+    return 0
 end
