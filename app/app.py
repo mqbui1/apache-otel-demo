243955b282481c76a7321716aac30e39d059d39e
Marc Bui <mqbui1@gmail.com>
2026-02-10 11:39:53 -0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -8,7 +8,6 @@ from opentelemetry.instrumentation.flask import FlaskInstrumentor
 
 app = Flask(__name__)
 
-# Setup tracing
 resource = Resource(attributes={"service.name": "flask-app"})
 trace.set_tracer_provider(TracerProvider(resource=resource))
 tracer = trace.get_tracer(__name__)
@@ -16,7 +15,6 @@ tracer = trace.get_tracer(__name__)
 otlp_exporter = OTLPSpanExporter(endpoint="http://otel-collector:4318/v1/traces", insecure=True)
 trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(otlp_exporter))
 
-# Auto-instrument Flask
 FlaskInstrumentor().instrument_app(app)
 
 @app.route("/")
