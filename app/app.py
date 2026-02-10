ee0f6ef71483a792aea8eb026d8f60acb61ebdac
Marc Bui <mqbui1@gmail.com>
2026-02-09 21:29:30 -0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,14 +4,10 @@ import time
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    time.sleep(0.1)
-    return "Hello from backend service\n"
+def hello():
+    return "Hello from Flask!"
 
 @app.route("/slow")
 def slow():
     time.sleep(1)
-    return "Slow endpoint\n"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    return "Slow response"
