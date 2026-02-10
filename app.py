e5556378160b4a0a2dd78d99ab3feb2bafc1ce52
Marc Bui <mqbui1@gmail.com>
2026-02-09 22:16:04 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from flask import Flask
-import time
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask!"
-
-@app.route("/slow")
-def slow():
-    time.sleep(1)
-    return "Slow response"
+    return "Hello, OpenTelemetry!"
