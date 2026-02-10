cc48bdc335bd06c4c9338546368e8652b4b61f8a
Marc Bui <mqbui1@gmail.com>
2026-02-10 10:57:11 -0800

    Update otel.lua

--- apache/otel.lua
+++ apache/otel.lua
@@ -1,42 +1,35 @@
 -- otel.lua
 local http = require("socket.http")
 local ltn12 = require("ltn12")
-local dkjson = require("dkjson")
-local socket = require("socket")
+local json = require("dkjson")
 
--- Utility: generate random hex IDs for trace/span
-local function random_hex(length)
-    local chars = "0123456789abcdef"
-    local s = ""
-    for i = 1, length do
-        local idx = math.random(1, #chars)
-        s = s .. chars:sub(idx, idx)
-    end
-    return s
-end
+-- Helper to send a single span to OTEL Collector
+local function send_span(trace_id, span_id, name, method, url)
+    local span = {
+        traceId = trace_id,
+        spanId = span_id,
+        name = name,
+        kind = 1,  -- 1 = SERVER
+        startTimeUnixNano = os.time() * 1e9,
+        endTimeUnixNano = (os.time() * 1e9) + 1e6,  -- +1ms for example
+        attributes = {
+            { key = "http.method", value = { stringValue = method } },
+            { key = "http.url", value = { stringValue = url } }
+        }
+    }
 
-local function generate_trace_id()
-    return random_hex(32)
-end
-
-local function generate_span_id()
-    return random_hex(16)
-end
-
--- Send a trace to OTEL Collector OTLP HTTP endpoint
-local function send_trace(trace)
-    local body = dkjson.encode({
-        resourceSpans = {{
-            resource = {attributes={{key="service.name", value={stringValue="apache-demo"}}}},
-            instrumentationLibrarySpans = {{
-                instrumentationLibrary = {name="lua-apache", version="0.1"},
-                spans = {trace}
-            }}
-        }}
+    local body = json.encode({
+        resourceSpans = {
+            {
+                instrumentationLibrarySpans = {
+                    { spans = { span } }
+                }
+            }
+        }
     })
 
-    local response = {}
-    local ok, err = http.request{
+    local resp_body = {}
+    local res, code = http.request{
         url = "http://otel-collector:4318/v1/traces",
         method = "POST",
         headers = {
@@ -44,37 +37,22 @@ local function send_trace(trace)
             ["Content-Length"] = tostring(#body)
         },
         source = ltn12.source.string(body),
-        sink = ltn12.sink.table(response)
+        sink = ltn12.sink.table(resp_body)
     }
 
-    if not ok then
-        ngx.log(ngx.ERR, "Failed to send trace: ", err)
+    if code ~= 200 then
+        ngx.log(ngx.ERR, "OTEL export failed: ", code, " - ", table.concat(resp_body))
     end
 end
 
--- Apache Lua hook
-function trace_request(r)
-    local trace_id = generate_trace_id()
-    local span_id = generate_span_id()
-    local timestamp = socket.gettime()
+-- Hook to Apache requests
+function trace_request()
+    local method = ngx.var.request_method
+    local uri = ngx.var.request_uri
+    local trace_id = tostring(math.random(1, 0xFFFFFFFFFFFFFFFF))  -- simple example
+    local span_id  = tostring(math.random(1, 0xFFFFFFFFFFFFFFFF))
 
-    local trace = {
-        traceId = trace_id,
-        spanId = span_id,
-        name = r.uri,
-        kind = 1,  -- 1 = CLIENT, 2 = SERVER
-        startTimeUnixNano = math.floor(timestamp * 1e9),
-        endTimeUnixNano = math.floor((timestamp + 0.001) * 1e9),  -- fake short duration
-        attributes = {
-            {key="http.method", value={stringValue=r.method}},
-            {key="http.url", value={stringValue=r.uri}}
-        }
-    }
+    send_span(trace_id, span_id, uri, method, uri)
 
-    -- Log locally for debugging
-    r:err("OTEL TRACE: " .. dkjson.encode(trace))
-    -- Send to OTEL Collector
-    send_trace(trace)
-
-    return 0  -- must return numeric for phase fixups
+    return 0  -- Fixups phase requires numeric return
 end
